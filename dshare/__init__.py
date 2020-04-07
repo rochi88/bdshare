21f5013599058bede2bc171ff2d169cb75f28ff9
rochi88 <raisul.me@gmail.com>
2020-04-07 20:22:40 +0600

    added initial functionality

--- dshare/__init__.py
+++ dshare/__init__.py
@@ -6,11 +6,4 @@ __author__ = 'Raisul Islam'
 """
 for trading data
 """
-from dshare.stock.trading import (get_hist_data, get_tick_data,
-                                   get_today_all, get_realtime_quotes,
-                                   get_h_data, get_today_ticks,
-                                   get_index, get_hists,
-                                   get_k_data, get_day_all,
-                                   get_sina_dd, bar, tick,
-                                   get_markets, quotes,
-                                   get_instrument, reset_instrument)
+from dshare.stock.trading import (get_hist_data, get_today_all)
---
+++ dshare/stock/trading.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+import datetime
+try:
+    from urllib.request import urlopen, Request
+except ImportError:
+    from urllib2 import urlopen, Request
+
+
+def get_hist_data(code=None, start=None, end=None,retry_count=3,pause=0.001):
+
+
+
+def get_today_all():
+
+
