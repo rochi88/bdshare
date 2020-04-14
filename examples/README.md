bd751ddbeab64b82423f9fb67a41a585664012b7
rochi88 <raisul.me@gmail.com>
2020-04-14 15:25:32 +0600

    updated a example for graph

---
+++ examples/README.md
@@ -0,0 +1,10 @@
+# Plotly.js-Flask Example
+A simple plotly.js example served with flask
+
+## Usage
+
+```bash
+$ python ex_flask.py
+```
+
+ * Running on http://0.0.0.0:9999/ (Press CTRL+C to quit)
---
+++ examples/ex_flask.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template
+from bdshare import get_basic_hist_data
+
+import plotly
+
+import json
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/')
+def index():
+    df = get_basic_hist_data('2019-03-01','2020-03-01','ACI')
+				
+    graphs = [
+        dict(
+            data=[
+                dict(
+                    x=df['date'],
+					open=df['open'],
+					high=df['high'],
+					low=df['low'],
+					close=df['close'],
+                    type='candlestick'
+                ),
+            ],
+            layout=dict(
+                title='first graph'
+            )
+        ),
+
+        dict(
+            data=[
+                dict(
+                    x=df['date'],
+                    y=df['close'],
+					mode='lines+markers',
+                    type='scatter'
+                ),
+            ],
+            layout=dict(
+                title='second graph'
+            )
+        )
+    ]
+				
+	# Add "ids" to each of the graphs to pass up to the client
+    # for templating
+    ids = ['graph-{}'.format(i+1) for i, _ in enumerate(graphs)]
+
+    # Convert the figures to JSON
+    # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
+    # objects to their JSON equivalents
+    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+	
+    return render_template('index.html', ids=ids, graphJSON=graphJSON)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9999)
