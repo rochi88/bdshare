a9bfa3c2a75edd72eb28f7708fdfb13fb93aaf02
Raisul Islam <raisul.me@gmail.com>
2023-03-12 18:21:11 +0600

    .

--- bdshare/util/vars.py
+++ bdshare/util/vars.py
@@ -9,6 +9,7 @@ DSE_AGM_URL = "Company_AGM.htm"
 DSE_LPE_URL = "latest_PE_all.php"
 DSE_NEWS_URL = "old_news.php"
 DSE_CLOSE_PRICE_URL = "dse_close_price_archive.php"
+DSE_COMPANY_LIST_URL = "company_listing.php"
 
 DSE_MARKET_INF_URL = "recent_market_information.php"
 DSE_MARKET_INF_MORE_URL = "recent_market_information_more.php"
