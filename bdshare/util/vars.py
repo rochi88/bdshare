dbb14de429255a09032b4675a42d618d495ac967
rochi88 <raisul.me@gmail.com>
2026-07-20 01:28:22 +0600

    fix: correct URL for top gainers to match updated naming convention

--- bdshare/util/vars.py
+++ bdshare/util/vars.py
@@ -21,5 +21,5 @@ DSE_MARKET_SUMMARY_URL = "market_summary.php"
 DSEX_INDEX_VALUE = "dseX_share.php"
 
 DSE_SECTOR_PERF_URL = "sector_performance.php"
-DSE_TOP_GAINERS_URL = "top_gainers.php"
+DSE_TOP_GAINERS_URL = "top_ten_gainer.php"
 
