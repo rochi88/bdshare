b02778d63c3dd13e9f4c9830db408c1c879d7548
rochi88 <raisul.me@gmail.com>
2026-09-25 16:01:13 +0600

    Update DSE URLs to point to the old site

--- bdshare/util/vars.py
+++ bdshare/util/vars.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
-DSE_URL = "https://dsebd.org/"
-DSE_ALT_URL = "https://dse.com.bd/"
+DSE_URL = "https://old.dsebd.org/"
+DSE_ALT_URL = "https://old.dse.com.bd/"
 
 DSE_LSP_URL = "latest_share_price_scroll_l.php"
 DSE_DEA_URL = "day_end_archive.php"
